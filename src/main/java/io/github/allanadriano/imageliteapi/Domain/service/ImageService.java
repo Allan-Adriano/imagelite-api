963a1d6e68cd847f294aa45d30caf94d5508cd01
Allan Adriano <allanadriano.n@gmail.com>
2026-04-18 09:18:29 -0300

    feat: cria a camada de serviço para a entidade Image

---
+++ src/main/java/io/github/allanadriano/imageliteapi/Domain/service/ImageService.java
@@ -0,0 +1,8 @@
+package io.github.allanadriano.imageliteapi.Domain.service;
+
+import io.github.allanadriano.imageliteapi.Domain.entity.Image;
+
+public interface ImageService {
+
+    Image save(Image image);
+}
---
+++ src/main/java/io/github/allanadriano/imageliteapi/application/images/ImageServiceImpl.java
@@ -0,0 +1,21 @@
+package io.github.allanadriano.imageliteapi.application.images;
+
+import io.github.allanadriano.imageliteapi.Domain.entity.Image;
+import io.github.allanadriano.imageliteapi.Domain.service.ImageService;
+import io.github.allanadriano.imageliteapi.infra.repository.ImageRepository;
+import lombok.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image) {
+        return repository.save(image);
+    }
+}
